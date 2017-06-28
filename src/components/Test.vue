c5c838e6ecd9436ee65812815bf5e7767fd68e90
unknown <18922860697@163.com>
2017-06-28 18:25:20 +0800

    test

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,6 +2,16 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Just a test</h2>
+    <span>{{ message }}</span>
+    <p><input v-model="message"></p>
+    <button v-bind:disabled="someDynamicCondition">Button</button>
+    <p v-if="seen">看到我了</p>
+    <li>
+    <li v-for="todo in todos">
+      {{ todo.text }}
+    </li>
+  </li>
+  <p><button v-on:click.once="reverseMessage">逆转消息</button></p>
   </div>
 </template>
 
@@ -10,9 +20,23 @@ export default {
   name: 'test',
   data () {
     return {
-      msg: '哈喽，我是测试页'
+      msg: '哈喽，我是测试页',
+      message: '当前时间：' + new Date(),
+      seen: true,
+      someDynamicCondition: false,
+      todos: [
+      { text: '学习 JavaScript' },
+      { text: '学习 Vue' },
+      { text: '整个牛项目' }
+    ]
+
     }
-  }
+  },
+  methods:{
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('');
+      }
+  },
 }
 </script>
 
