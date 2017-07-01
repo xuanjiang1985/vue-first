e6d92a93734521721d38c0ca2977a6722cd24c4f
周刚 <zhougang@zhougangdeMacBook-Air.local>
2017-07-01 12:02:28 +0800

    add vue-resource

--- src/components/my.vue
+++ src/components/my.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="hello">
-    <h1>404. Not Found!</h1>
+    <h1>my component {{ num }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+  	return {num: 1000}
+  }
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import my from './components/my'
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.component('my-com',my)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
